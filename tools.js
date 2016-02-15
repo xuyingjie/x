f7e4749474a1df678ff4735e273eb81069b40d3b
xuyingjie <xuyingjie@outlook.com>
2016-02-15 23:42:04 +0800

    Web Cryptography HMAC

--- tools.js
+++ tools.js
@@ -1,5 +1,5 @@
 const bucket = 'whitehairpin'
-const url = `https://${bucket}.oss-cn-beijing.aliyuncs.com`
+const url = `http://${bucket}.oss-cn-beijing.aliyuncs.com`
 export const dns = `http://${bucket}.img-cn-beijing.aliyuncs.com`
 
 export function get(key, { responseType, progress } = {}) {
@@ -23,19 +23,20 @@ export function get(key, { responseType, progress } = {}) {
 
 export function upload(key, data, { progress } = {}) {
   return new Promise(resolve => {
+    form(key, data).then(out => {
+      var xhr = new XMLHttpRequest()
+      xhr.open('POST', url)
 
-    var xhr = new XMLHttpRequest()
-    xhr.open('POST', url)
+      if (progress) xhr.upload.onprogress = (e) => {
+        if (e.lengthComputable) progress.style.width = e.loaded / e.total * 100 + '%'
+      }
 
-    if (progress) xhr.upload.onprogress = (e) => {
-      if (e.lengthComputable) progress.style.width = e.loaded / e.total * 100 + '%'
-    }
+      xhr.onload = () => {
+        resolve(xhr.response)
+      }
 
-    xhr.onload = () => {
-      resolve(xhr.response)
-    }
-
-    xhr.send(form(key, data))
+      xhr.send(out)
+    })
   })
 }
 
@@ -61,20 +62,23 @@ export function form(key, data) {
     ],
   }
   const policy = btoa(JSON.stringify(policyJson))
-  const signature = b64_hmac_sha1(SK, policy)
 
-  const formData = new FormData()
-  formData.append('key', key)
-  formData.append('x-oss-object-acl', 'public-read')
-  formData.append('Content-Type', 'application/octet-stream')
-  formData.append('Cache-Control', cache)
-  formData.append('OSSAccessKeyId', AK)
-  formData.append('policy', policy)
-  formData.append('signature', signature)
+  return new Promise(resolve => {
+    b64HmacSHA1(SK, policy).then(signature => {
+      const formData = new FormData()
+      formData.append('key', key)
+      formData.append('x-oss-object-acl', 'public-read')
+      formData.append('Content-Type', 'application/octet-stream')
+      formData.append('Cache-Control', cache)
+      formData.append('OSSAccessKeyId', AK)
+      formData.append('policy', policy)
+      formData.append('signature', signature)
 
-  // 文件或文本内容，必须是表单中的最后一个域。
-  formData.append('file', new Blob([data]))
-  return formData
+      // 文件或文本内容，必须是表单中的最后一个域。
+      formData.append('file', new Blob([data]))
+      resolve(formData)
+    })
+  })
 }
 
 
@@ -83,7 +87,8 @@ export function arrayBufferToStr(buf) {
 }
 export function strToArrayBuffer(str) {
   // return Uint16Array.from([...str], s => s.charCodeAt(0)).buffer
-  return new Uint16Array([...str].map(s => s.charCodeAt(0))).buffer
+  // return new Uint16Array([...str].map(s => s.charCodeAt(0))).buffer
+  return new TextEncoder('utf-16').encode(str)
 }
 
 //
@@ -206,113 +211,16 @@ export function decrypt(passwd, data) {
   })
 }
 
-export function b64_hmac_sha1(k, d, _p, _z) {
+export function b64HmacSHA1(key, str) {
+  var keyBuf = new TextEncoder('utf-8').encode(key)
+  var buf = new TextEncoder('utf-8').encode(str)
 
-  // heavily optimized and compressed version of http://pajhome.org.uk/crypt/md5/sha1.js
-  // _p = b64pad, _z = character size; not used here but I left them available just in case
-  if (!_p) {
-    _p = '='
-  }
-  if (!_z) {
-    _z = 8
-  }
-  function _f(t, b, c, d) {
-    if (t < 20) {
-      return (b & c) | ((~b) & d)
-    }
-    if (t < 40) {
-      return b ^ c ^ d
-    }
-    if (t < 60) {
-      return (b & c) | (b & d) | (c & d)
-    }
-    return b ^ c ^ d
-  }
-  function _k(t) {
-    return (t < 20) ? 1518500249 : (t < 40) ? 1859775393 : (t < 60) ? -1894007588 : -899497514
-  }
-  function _s(x, y) {
-    var l = (x & 0xFFFF) + (y & 0xFFFF),
-      m = (x >> 16) + (y >> 16) + (l >> 16)
-    return (m << 16) | (l & 0xFFFF)
-  }
-  function _r(n, c) {
-    return (n << c) | (n >>> (32 - c))
-  }
-  function _c(x, l) {
-    x[l >> 5] |= 0x80 << (24 - l % 32)
-    x[((l + 64 >> 9) << 4) + 15] = l;var w = [80],
-      a = 1732584193,
-      b = -271733879,
-      c = -1732584194,
-      d = 271733878,
-      e = -1009589776
-    for (var i = 0; i < x.length; i += 16) {
-      var o = a,
-        p = b,
-        q = c,
-        r = d,
-        s = e
-      for (var j = 0; j < 80; j++) {
-        if (j < 16) {
-          w[j] = x[i + j]
-        } else {
-          w[j] = _r(w[j - 3] ^ w[j - 8] ^ w[j - 14] ^ w[j - 16], 1)
-        }
-        var t = _s(_s(_r(a, 5), _f(j, b, c, d)), _s(_s(e, w[j]), _k(j)))
-        e = d
-        d = c
-        c = _r(b, 30)
-        b = a
-        a = t
-      }
-      a = _s(a, o)
-      b = _s(b, p)
-      c = _s(c, q)
-      d = _s(d, r)
-      e = _s(e, s)
-    }
-    return [a, b, c, d, e]
-  }
-  function _b(s) {
-    var b = [],
-      m = (1 << _z) - 1
-    for (var i = 0; i < s.length * _z; i += _z) {
-      b[i >> 5] |= (s.charCodeAt(i / 8) & m) << (32 - _z - i % 32)
-    }
-    return b
-  }
-  function _h(k, d) {
-    var b = _b(k)
-    if (b.length > 16) {
-      b = _c(b, k.length * _z)
-    }
-    var p = [16],
-      o = [16]
-    for (var i = 0; i < 16; i++) {
-      p[i] = b[i] ^ 0x36363636
-      o[i] = b[i] ^ 0x5C5C5C5C
-    }
-    var h = _c(p.concat(_b(d)), 512 + d.length * _z)
-    return _c(o.concat(h), 512 + 160)
-  }
-  function _n(b) {
-    var t = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/',
-      s = ''
-    for (var i = 0; i < b.length * 4; i += 3) {
-      var r = (((b[i >> 2] >> 8 * (3 - i % 4)) & 0xFF) << 16) | (((b[i + 1 >> 2] >> 8 * (3 - (i + 1) % 4)) & 0xFF) << 8) | ((b[i + 2 >> 2] >> 8 * (3 - (i + 2) % 4)) & 0xFF)
-      for (var j = 0; j < 4; j++) {
-        if (i * 8 + j * 6 > b.length * 32) {
-          s += _p
-        } else {
-          s += t.charAt((r >> 6 * (3 - j)) & 0x3F)
-        }
-      }
-    }
-    return s
-  }
-  function _x(k, d) {
-    return _n(_h(k, d))
-  }
-  return _x(k, d)
+  return new Promise(resolve => {
+    var hmacSha1 = {name: 'hmac', hash: {name: 'sha-1'}}
+    crypto.subtle.importKey('raw', keyBuf, hmacSha1, true, ['sign', 'verify']).then(out => {
+      crypto.subtle.sign(hmacSha1, out, buf).then(result => {
+        resolve(btoa(String.fromCharCode.apply(null, new Uint8Array(result))))
+      })
+    })
+  })
 }
